875bf8924242352a000240f10a52063c7cb14ca1
GabrielfSilveiraDev <gabrielfornaza@yahoo.com.br>
2026-04-30 01:49:00 -0300

    Hardcoded azure address

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // SUMMARY: Creates the base Axios instance for API communication.
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5101/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://api-gestaluguel-gabriel-hxdmg4bfataxhzfg.brazilsouth-01.azurewebsites.net/api',
   headers: {
     'Content-Type': 'application/json',
   },
